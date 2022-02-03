e2bc58f94a45f3bf24f03586cd9eea464c1cc681
Opilane TTHK <opilane@tthk.ee>
2022-02-03 10:05:28 +0200

    new

---
+++ Toolivahendus2/Models/ToolidValmistusel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Toolivahendus2.Models
+{
+    public class ToolidValmistusel
+    {
+       public int Id { get; set; }
+       public string Firmanimi { get; set; }
+       [Required]
+       public int ValminudKogus { get; set; } = -0;
+    }
+}
